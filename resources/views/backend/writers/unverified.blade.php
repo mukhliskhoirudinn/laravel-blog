3b3eb3215202e267a92655dcfec76da49c8bedd1
mukhliskhoirudinn <mukhliskhoirudinn@gmail.com>
2024-10-30 06:43:08 +0700

    update halaman unverified dan menambahkan tabel user di migration

--- resources/views/backend/writers/unverified.blade.php
+++ resources/views/backend/writers/unverified.blade.php
@@ -1,4 +1,22 @@
-<h2>
-    Unverified Writers: {{ auth()->user()->email }}, Please wait or contact owner:
-    <a href="mailto:{{ $owner_email }}">{{ $owner_email }}</a>
-</h2>
+@extends('layouts.app')
+
+@section('title', 'Writers')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <x-card icon="tag" title="Writers">
+                    <div class="table-responsive-sm">
+                        <h2>
+                            Unverified Writers: {{ auth()->user()->email }},<br>
+                            Please wait or contact owner:
+                            <a href="mailto:{{ $owner_email }}">{{ $owner_email }}</a>
+                        </h2>
+                    </div>
+                </x-card>
+            </div>
+        </div>
+    </div>
+
+@endsection
